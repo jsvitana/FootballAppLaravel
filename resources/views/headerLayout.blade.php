b67940c56644495ea9f257432471eed7aba2bb07
Jason Schopp <jschopp99@southhills.edu>
2019-11-12 10:00:03 -0500

    Create headerLayout.blade.php

---
+++ resources/views/headerLayout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title></title>
+</head>
+<body>
+    <nav id="headerForAllPages">
+        <div  class="nav-wrapper">
+          <a href="/home" class="brand-logo">Home</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="/teamscores">Team Scores</a></li>
+            <li><a href="">Nothing yet</a></li>
+            <li><a href="">Nothing yet</a></li>
+          </ul>
+        </div>
+      </nav>
+</body>
+</html>
